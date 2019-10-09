41258e47e847689adb831f088582df657de47508
ARKADE08 <yorhansilvadavid@gmail.com>
2019-10-08 22:16:23 -0300

    prototipo atualizado

---
+++ Prototipo/Assets/Scripts/Arma.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arma: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string arma;
+
+    public int QuantidadeDeBalas = 30;
+    public int Cartuchos = 5;
+
+    public Transform Disparador;
+    public GameObject Muniçao;
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && QuantidadeDeBalas > 0) 
+            shoot();
+    }
+
+   
+    void shoot()
+
+    {
+        Instantiate(Muniçao, Disparador.position, Disparador.rotation);
+    }
+
+
+}
+
+ 
---
+++ Prototipo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Animator Animaçao;
+    private Rigidbody2D Gravidade;
+
+   
+
+
+    public Transform Pisando;
+    [HideInInspector] public bool TocandoChao = false;
+
+    public float Velocidade;
+    public float ForçaDoPulo = 5f;
+    [HideInInspector] public bool viradoDireita = true;
+
+    public Image vida;
+    private MensagemControle MC;
+
+    
+
+
+    void Start()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+        float translationY = Input.GetAxis("Vertical");
+        float translationX = Input.GetAxis("Horizontal") * Velocidade;
+        transform.Translate(translationX, translationY, 0);
+        transform.Rotate(0, 0, 0);
+
+        Animaçao.SetFloat("velocidade", translationX);
+        {
+            if (Velocidade > 1)
+            {
+                if (!TocandoChao)
+
+                    Animaçao.SetFloat("corre", translationX);
+            }
+            if (translationX != 0 && TocandoChao)
+            {
+                if (!TocandoChao)
+                {
+                    Animaçao.SetTrigger("pulando");
+                }
+
+            }
+        }
+        Animaçao.SetFloat("velocidade", translationX);
+        {
+
+        }
+    }
+
+    void Update()
+    {
+
+
+
+    }
+
+        void Flip()
+        {
+            viradoDireita = !viradoDireita;
+            Vector3 escala = transform.localScale;
+            escala.x *= -1;
+            transform.localScale = escala;
+        }
+
+        void SubtraiVida()
+        {
+            vida.fillAmount -= 0.1f;
+            if (vida.fillAmount <= 0)
+                MC.GameOver();
+            Destroy(gameObject);
+        }
+
+    
+
+}       
